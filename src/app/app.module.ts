c3ac5b30243198c32bf58054a8d513e2546b1b81
irfanali <irfu3844@gmail.com>
2023-02-23 11:33:54 +0530

    23-02-2023 BROTOTYPE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms';
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,15 @@ import { BottonComponent } from './components/botton/botton.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+const appRoutes:Routes=[
+  {path:'',component:TasksComponent},
+  {path:'about',component:AboutComponent}
+
+]
 
 @NgModule({
   declarations: [
@@ -19,14 +29,18 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
     BottonComponent,
     TasksComponent,
     TaskItemComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.
+    forRoot(appRoutes,{enableTracing:true})
 
   ],
   providers: [],
